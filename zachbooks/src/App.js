7c812c4fda2585986ba0fe873e91072b41f5e280
Zach <zirachdev@gmail.com>
2022-04-16 16:17:14 +0800

    App set up for components

--- zachbooks/src/App.js
+++ zachbooks/src/App.js
@@ -1,7 +1,25 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import CreateBook from "./components/CreateBook";
+import ShowBookDetails from "./components/ShowBookDetails";
+import ShowBookList from "./components/ShowBookList";
+import UpdateBookInfo from "./components/UpdateBookInfo";
+
+//CSS
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={ShowBookList} />
+        <Route exact path="/create-book" component={CreateBook} />
+        <Route exact path="/edit-book/:id" component={UpdateBookInfo} />
+        <Route exact path="/show-book/:id" component={ShowBookDetails} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
